66aad5ff768f76fafc120ec73e1436ce6d646b17
AlinaCalistru <73351416+AlinaCalistru@users.noreply.github.com>
2022-03-12 17:17:19 +0000

    final version of invoice generator

--- index.js
+++ index.js
@@ -1,48 +1,81 @@
 let services = [
-  { name: "Wash Car", price: 10 },
+   { name: "Wash Car", price: 10 },
   { name: "Mow Lawn", price: 20 },
-  { name: "Pull Weeds", price: 30 }
+  { name: "Pull Weeds", price: 30 },
 ];
 
-function addServices() {
+let selectedServices = [];
+let total = 0;
+
+function addServicesBtns() {
   const serviceContainer = document.querySelector(".buttons");
-  // alert("test");
-  // serviceContainer.innerHTML = ""
   for (let i = 0; i < services.length; i++) {
     console.log(i);
     const addServices = document.createElement("button");
-    addServices.classList.add("btn-service")
-    addServices.dataset.name = services[i].name;
-    addServices.dataset.price = services[i].price;
+    addServices.classList.add("btn-service");
     addServices.dataset.index = i;
     addServices.textContent = services[i].name + " $" + services[i].price;
     serviceContainer.append(addServices);
   }
 }
 
-addServices();
-
+addServicesBtns();
 
 const serviceOrders = document.querySelectorAll(".buttons");
 
 Array.from(serviceOrders).forEach(function (element) {
-    element.addEventListener("click", addToOrder);
-  });
+  element.addEventListener("click", addService);
+});
 
-
-function addToOrder(el){
-    const orderEl = document.createElement("li");
-    let ulEl= document.querySelector(".selected-services");
-    orderEl.textContent = el.target.dataset.name;
-    ulEl.append(orderEl);
+function addService(el) {
+  const index = el.target.dataset.index;
+  if (!selectedServices.includes(index)) {
+    selectedServices.push(index);
+    total += services[index].price;
+    renderServices();
+  }
 }
 
+function renderServices() {
+  let ulEl = document.querySelector(".selected-services");
+  ulEl.innerHTML = "";
+  Array.from(selectedServices).forEach(function (index) {
+    console.log(index);
+    const orderEl = document.createElement("li");
+    orderEl.classList.add("service");
+    const orderNameEl = document.createElement("div");
+    const orderPriceEl = document.createElement("div");
+    const removeEl = document.createElement("a");
+    removeEl.dataset.index = index;
+    removeEl.addEventListener("click",  removeService);
+    removeEl.innerHTML = " Remove";
+    removeEl.classList.add("remove");
+    orderNameEl.innerHTML = services[index].name;
+    orderNameEl.append(removeEl);
+    orderPriceEl.innerHTML = "<span>$</span>" + services[index].price;
+    orderEl.append(orderNameEl);
+    orderEl.append(orderPriceEl);
+    ulEl.append(orderEl);
+  });
+  const totalEl = document.querySelector(".total");
+  totalEl.innerHTML = "<span>$</span>" + total;
+}
 
+function removeService(el){
+  console.log("aaaaa")
+  console.log(selectedServices);
+  el.preventDefault();
+  let idx = el.target.dataset.index;
+  console.log(idx)
+  let findItem = selectedServices.indexOf(idx);
+  selectedServices.splice(findItem, 1);
+  total -= services[idx].price;
+  renderServices();
+}
 
-
-// //   reset button 
-// function resetItems(){
-//         document.getElementById("btn-send").onclick = function() {
-//         document.getElementById("button").value = "";
-// }
-// };
+const btnSend = document.getElementById("btn-send");
+btnSend.addEventListener("click", function(){
+  total = 0;
+  selectedServices = [];
+  renderServices();
+})
